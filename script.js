ebc68f7218074da7935e0361ad807662fe7a5844
Ashar zama Khan <asharzamakhan01@gmail.com>
2023-04-14 16:05:47 +0530

    changes made

--- script.js
+++
@@ -1,9 +0,0 @@
-function showsidebar() {
-  const sidebar = document.getElementById("sidebar");
-  const classes = sidebar.classList;
-  const translate = "translate-x-96";
-
-  classes.contains(translate)
-    ? classes.remove(translate)
-    : classes.add(translate);
-}
